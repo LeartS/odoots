866068d1c34ec168e6ab0d959c05424b9d6469e1
Leonardo Donelli <learts92@gmail.com>
2019-05-12 15:40:35 +0200

    Implement JSON-RPC and odoo methods call

--- README.md
+++ README.md
@@ -11,10 +11,57 @@ odoots is a typescript client for odoo's JSON-RCP API.
 
 ## Usage
 
+### Initialize client
+Create and initialize the client by providing the odoo host and the database
+name to use.
 ```typescript
 import * as odoots from 'tsodoo'
 
 const odoo = new odoots('https://runbot.odoo.com', 'dbname')
-odoo.login('user', 'password').then(loggedUser => console.log(loggedUser))
-)
 ```
+
+### Login
+
+Before calling model methods, you'll have to login using the credentials of
+an internal odoo user.
+
+```typescript
+odoo.login('user', 'password').then(loggedUser => console.log(loggedUser))
+```
+The login method returns some data about the logged in user:
+```javascript
+{ uid: 2,
+  partnerId: 3,
+  sessionId: '0ab5c8817ca4c8bed0b6dff42606c27e3c3eee93',
+  companyId: 1,
+  name: 'Administrator',
+  username: 'admin',
+  isAdmin: true,
+  isSuperuser: undefined,
+  context: { lang: 'it_IT', tz: false, uid: 2 } }
+```
+
+### Call methods on odoo models
+
+```typescript
+odoo.call(model, method, args?, kwargs?, options?)
+```
+
+for example:
+
+```typescript
+odoo.call('res.users', 'search_read', [[[1, '=', 1]], ['name', 'login']])
+```
+
+returns a list of all users with the provided fields:
+
+```javascript
+[ { name: 'Administrator', login: 'admin', id: 2 } ]
+```
+(note that odoo' `search_read` always returns the `id` field)
+
+Right now, the only option is `includeContext: boolean`.
+If `true` (the default) the user context returned during login
+is automatically added to the keyword arguments of the call. This is important
+because the language and timezone values in the user context can change the
+return values of a call, which become therefore context-dependant.
--- src/odoots.ts
+++ src/odoots.ts
@@ -1,6 +1,8 @@
 import * as nodefetch from 'node-fetch'
 import debug = require('debug')
 
+import * as jsonrpc from 'jsonrpc-lite'
+
 
 const debugHTTP = debug('odoots:http')
 
@@ -70,46 +72,111 @@ export interface LoggedUser {
     context: UserContext
 }
 
+class OdooError extends Error {
+    name = 'OdooError'
+    constructor (public odooErrorName: string, public odooErrorMessage: string) {
+        super(`${odooErrorName}: ${odooErrorMessage}`)
+    }
+}
+
+function buildOdooErrorFromJRPCErrorResult (jsonRpcError: any): OdooError {
+    return new OdooError(jsonRpcError.data.name, jsonRpcError.data.message)
+}
+
+function findSessionCookie (setCookie: string | null): string | undefined {
+    if (!setCookie) {
+        return undefined
+    }
+    const cookieParts = setCookie.split(';')
+    return cookieParts.find(cp => cp.startsWith('session_id='))
+}
+
+/**
+ * Fix non-spec compliant odoo JSON-RPC responses
+ *
+ * According to JSON-RPC spec
+ * (https://www.jsonrpc.org/specification#response_object)
+ * the `result` parameter is required in sucessfull response objects.
+ *
+ * Odoo breaks the spec by returning response without `result` in case of
+ * successfull method calls with no return value. This methods fixes
+ * that by adding a fixed result value when is undefined.
+ */
+function patchOdooJRPCResponse (odooResponse: any) {
+    if (odooResponse.result || odooResponse.error) {
+        return odooResponse
+    }
+    return {result: '__OK__', ...odooResponse}
+}
+
 export default class Odoots {
 
     private fetch: typeof fetch
     public loggedUser: LoggedUser | undefined
+    private sessionCookie: string | undefined
+    // Incremental request id to use for JSON-RPC
+    private requestId: number = 1
 
     constructor (private host: string, private dbname: string) {
         this.fetch = getFetchImplementation()
         this.loggedUser = undefined
     }
 
-    private async jrpcCall (path: string, data: any): Promise<any> {
+    /**
+     * Makes a JSON-RPC 2.0 request
+     *
+     * Note that odoo's JSON-RPC implementation is a little strange: it ignores
+     * the `method` member of the JSON-RPC request object, and uses instead
+     * different endpoints for different stuff.
+     *
+     * Note that there are 3 levels of `methods` params,
+     * do not mix them up:
+     * - HTTP method (e.g. GET, POST, HEAD etc)
+     * - JSON-RPC request method (ignored by odoo JSON-RPC handling)
+     * - Odoo model method (e.g. update, search_read, search etc.)
+     */
+    private async makeJRPCRequest (path: string, params: object): Promise<any> {
+        const headers: any = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+        if (this.loggedUser) {
+            headers['Cookie'] = this.sessionCookie
+        }
+        // Creates JSON-RPC request object according to JSON-RPC 2.0 spec
+        // Odoo ignores the 'method' key so we just put `call` there.
+        const jrpcBody = jsonrpc.request(this.requestId++, 'call', params)
         const response = await this.fetch(this.host + path, {
             method: 'post',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify(data),
+            headers: headers,
+            body: jrpcBody
         })
-        let jrpcResponse = await response.json()
-        if (jrpcResponse.error) {
-            throw new OdooRPCError({
-                message: `${jrpcResponse.error.message}: ${jrpcResponse.error.data.message}`,
-                type: jrpcResponse.error.data.exception_type,
-                name: jrpcResponse.error.data.name,
-                debug: jrpcResponse.error.data.debug,
-            })
+        const sessionCookie = findSessionCookie(response.headers.get('set-cookie'))
+        if (sessionCookie) {
+            this.sessionCookie = sessionCookie
         }
-        return jrpcResponse.result
+        const t = await response.json()
+        const parsedRes = jsonrpc.parseObject(patchOdooJRPCResponse(t))
+        if (parsedRes.type === 'success') {
+            return parsedRes.payload.result
+        } else if (parsedRes.type === 'error') {
+            throw buildOdooErrorFromJRPCErrorResult(parsedRes.payload.error)
+        } else if (parsedRes.type === 'invalid') {
+            throw new Error('Odoo returned an invalid JSON-RPC response')
+        } else {
+            throw new Error('unexpected JSON-RPC response object')
+        }
     }
 
-
     async login (username: string, password: string): Promise<LoggedUser> {
-        const response = await this.jrpcCall('/web/session/authenticate', {
-            params: {
+        const response = await this.makeJRPCRequest(
+            '/web/session/authenticate',
+            {
                 db: this.dbname,
                 login: username,
                 password: password,
             }
-        })
+        )
         // In Odoo 10 invalid credentials don't return any kind of error,
         // just unset user values.
         if (!response.uid) {
@@ -134,6 +201,30 @@ export default class Odoots {
         return this.loggedUser
     }
 
+    /**
+     * Calls method of an odoo model through JSON-RPC
+     */
+    async call (
+        model: string,
+        method: string,
+        args: any[] = [],
+        kwargs: object = {},
+        options: {includeContext: boolean} = {includeContext: true}
+    ) {
+        if (!this.loggedUser) {
+            throw new Error('You must be logged in to call methods')
+        }
+        const kwargsFull: any = {...kwargs}
+        if (options.includeContext) {
+            kwargsFull.context = this.loggedUser.context
+        }
+        return this.makeJRPCRequest('/web/dataset/call_kw', {
+            model: model,
+            method: method,
+            args: args,
+            kwargs: kwargs
+        })
+    }
 }
 
 module.exports = Odoots
